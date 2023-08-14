02fb5bae31c1e6f72405b9457970cdd5e1992ec0
hawi-me <hawiabdi41@gmail.com>
2023-08-14 11:51:41 +0300

    ecommercesite

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/"> SpareStore</a>
+  
+      </header>
+      <main>
+          List products
+        </main>
+    </div>
+  );
+}
+
+export default App;
